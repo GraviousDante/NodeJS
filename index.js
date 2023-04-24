6dbcdaef7d4cc585672db6d36b7f506981b79732
GraviousDante <toankhanh2003@gmail.com>
2023-04-24 19:56:12 +0700

    NodeJS

--- index.js
+++ index.js
@@ -4,21 +4,17 @@ const bodyParser = require('body-parser');
 const app = express();
 const session = require("express-session");
 const http = require('http');
+const {connect} = require("mongoose");
+const {listen} = require("express/lib/application");
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World');
-});
-
-mongoose.connect('mongodb://localhost/T2203E', {useNewUrlParser: true})
+connect('mongodb://localhost/T2203E', {useNewUrlParser: true})
     .then(() => console.log('MongoDB connected...'))
     .catch(err => console.log(err));
 
-server.listen(port, hostname, () => {
+listen(port, hostname, () => {
     console.log(`Server running...`);
 });
 
